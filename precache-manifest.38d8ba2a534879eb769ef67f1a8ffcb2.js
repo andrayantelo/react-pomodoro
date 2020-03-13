c677f5468d7dc95fff6cdeb8814f8f9491b607cf
andrayantelo <andrayantelo@gmail.com>
2020-03-13 16:48:28 -0700

    Updates

---
+++ precache-manifest.38d8ba2a534879eb769ef67f1a8ffcb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17f94818f16fad39a65fddb1d884d8d",
+    "url": "/react-pomodoro/index.html"
+  },
+  {
+    "revision": "f2d4bd891c2bfc491f9e",
+    "url": "/react-pomodoro/static/css/main.fa3a29f5.chunk.css"
+  },
+  {
+    "revision": "b4b06bdf75d76bdb7ce2",
+    "url": "/react-pomodoro/static/js/2.62067395.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-pomodoro/static/js/2.62067395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d4bd891c2bfc491f9e",
+    "url": "/react-pomodoro/static/js/main.ec0c18e5.chunk.js"
+  },
+  {
+    "revision": "6d4ec33dcac0b16e99ac",
+    "url": "/react-pomodoro/static/js/runtime-main.9cd155da.js"
+  }
+]);
